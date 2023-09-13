058b10f4367f916c30f6429ea2009965dcabf78d
Safarmatova <safarmatovaguli4@gmail.com>
2023-09-13 16:01:35 +0500

    Search and a Selact

--- js/index.js
+++ js/index.js
@@ -113,3 +113,173 @@ fetch('https://restcountries.com/v3.1/all')
     })
 })
 .catch(function(error){console.log(error)})
+
+
+
+
+
+
+var elForm = document.querySelector('.hero-form');
+var elInp = document.querySelector('.hero-input');
+
+elForm.addEventListener('submit', function(evt){
+    evt.preventDefault()
+
+    var inpVelue = elInp.value;
+
+    if(inpVelue == ''){
+        window.location.reload()
+    }
+
+    fetch('https://restcountries.com/v3.1/name/' + inpVelue)
+    .then(function(res){return res.json()})
+    .then(function(data){
+        elList.innerHTML = '';
+
+        data.forEach(function(dav){
+            var newImg = document.createElement('img');
+        newImg.src = dav.flags.svg;
+        newImg.width = '264';
+        newImg.height = '160';
+        newImg.classList.add('hero-img');
+
+        var newTitle = document.createElement('h2');
+        newTitle.innerHTML = dav.name.common;
+        newTitle.classList.add('hero-title');
+
+
+        var newTextP = document.createElement('p');
+        newTextP.innerHTML = 'Population: ';
+        newTextP.classList.add('hero-text');
+
+        var newPopulation =document.createElement('span');
+        newPopulation.innerHTML = dav.population;
+        newPopulation.classList.add('hero-span');
+        newTextP.appendChild(newPopulation);
+
+
+
+        var newTextR = document.createElement('p');
+        newTextR.innerHTML = 'Region: ';
+        newTextR.classList.add('hero-text');
+
+        var newRegion =document.createElement('span');
+        newRegion .innerHTML = dav.region;
+        newRegion .classList.add('hero-span');
+        newTextR.appendChild(newRegion);
+
+
+
+        var newTextC = document.createElement('p');
+        newTextC.innerHTML = 'Capital: ';
+        newTextC.classList.add('hero-text');
+
+        var newCapital =document.createElement('span');
+        newCapital.innerHTML = dav.capital;
+        newCapital.classList.add('hero-span');
+        newTextC.appendChild(newCapital);
+
+
+        var newItem = document.createElement('li');
+        newItem.classList.add('hero-item');
+        newItem.appendChild(newImg);
+        newItem.appendChild(newTitle);
+        newItem.appendChild(newTextP);
+        newItem.appendChild(newTextR);
+        newItem.appendChild(newTextC);
+
+        elList.appendChild(newItem);
+
+        elHeaderBtn.addEventListener("click", function(){
+
+                newItem.classList.toggle("hero-item-dark");
+                newTitle.classList.toggle("hero-title-dark");
+                newTextP.classList.toggle("hero-text-dark");
+                newTextR.classList.toggle("hero-text-dark");
+                newTextC.classList.toggle("hero-text-dark");
+
+        })
+        })
+    })
+})
+
+
+
+
+
+var elSelect = document.querySelector('.hero-select');
+
+elSelect.addEventListener('change', function(evt){
+
+    fetch('https://restcountries.com/v3.1/region/' + evt.target.value)
+    .then(function(res){return res.json()})
+    .then(function(data){
+
+            elList.innerHTML = ""
+
+        data.forEach(function(dav){
+            var newImg = document.createElement('img');
+        newImg.src = dav.flags.svg;
+        newImg.width = '264';
+        newImg.height = '160';
+        newImg.classList.add('hero-img');
+
+        var newTitle = document.createElement('h2');
+        newTitle.innerHTML = dav.name.common;
+        newTitle.classList.add('hero-title');
+
+
+        var newTextP = document.createElement('p');
+        newTextP.innerHTML = 'Population: ';
+        newTextP.classList.add('hero-text');
+
+        var newPopulation =document.createElement('span');
+        newPopulation.innerHTML = dav.population;
+        newPopulation.classList.add('hero-span');
+        newTextP.appendChild(newPopulation);
+
+
+
+        var newTextR = document.createElement('p');
+        newTextR.innerHTML = 'Region: ';
+        newTextR.classList.add('hero-text');
+
+        var newRegion =document.createElement('span');
+        newRegion .innerHTML = dav.region;
+        newRegion .classList.add('hero-span');
+        newTextR.appendChild(newRegion);
+
+
+
+        var newTextC = document.createElement('p');
+        newTextC.innerHTML = 'Capital: ';
+        newTextC.classList.add('hero-text');
+
+        var newCapital =document.createElement('span');
+        newCapital.innerHTML = dav.capital;
+        newCapital.classList.add('hero-span');
+        newTextC.appendChild(newCapital);
+
+
+        var newItem = document.createElement('li');
+        newItem.classList.add('hero-item');
+        newItem.appendChild(newImg);
+        newItem.appendChild(newTitle);
+        newItem.appendChild(newTextP);
+        newItem.appendChild(newTextR);
+        newItem.appendChild(newTextC);
+
+        elList.appendChild(newItem);
+
+        elHeaderBtn.addEventListener("click", function(){
+
+                newItem.classList.toggle("hero-item-dark");
+                newTitle.classList.toggle("hero-title-dark");
+                newTextP.classList.toggle("hero-text-dark");
+                newTextR.classList.toggle("hero-text-dark");
+                newTextC.classList.toggle("hero-text-dark");
+
+        })
+        })
+})
+})
